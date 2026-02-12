5b28e49a2534631d045446342d3846a0107c5930
unnati1507 <unnatimishra1507@gmail.com>
2026-02-12 15:18:15 +0530

    Update

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const http=require('http');
 
 const myserver=http.createServer((req, res) => {
     if (req.url == '/') {
-        res.end('This is the Home Page');
+        res.end('<h1> ABES ENGINEERING COLLEGE </h1>');
     }
     else if (req.url == '/about'){
-        res.end('This is the About us Page');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<img src="https://abes.ac.in/assets/2.jpg" width="900">');
     }
     else if (req.url == '/contact'){
-        res.end('This is the Contact Page');
+        
+        res.end('<h1> Address: 19th KM Stone, NH-09, Ghaziabad, PIN - 201009. UP, India.</h1>');
     }
     else{
         res.end('404 Page Not Found');
