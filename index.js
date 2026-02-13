d392b31d23877e145ed5d999042447e06b0580a2
unnati1507 <unnatimishra1507@gmail.com>
2026-02-13 12:12:24 +0530

    update

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const http=require('http');
+// const http=require('http');
 
-const myserver=http.createServer((req, res) => {
-    if (req.url == '/') {
-        res.end('<h1> ABES ENGINEERING COLLEGE </h1>');
-    }
-    else if (req.url == '/about'){
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<img src="https://abes.ac.in/assets/2.jpg" width="900">');
-    }
-    else if (req.url == '/contact'){
+// const myserver=http.createServer((req, res) => {
+//     if (req.url == '/') {
+//         res.end('<h1> ABES ENGINEERING COLLEGE </h1>');
+//     }
+//     else if (req.url == '/about'){
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.end('<img src="https://abes.ac.in/assets/2.jpg" width="900">');
+//     }
+//     else if (req.url == '/contact'){
         
-        res.end('<h1> Address: 19th KM Stone, NH-09, Ghaziabad, PIN - 201009. UP, India.</h1>');
-    }
-    else{
-        res.end('404 Page Not Found');
-    }
-})
-myserver.listen(8000,()=>{
-    console.log('Server is running on port 8000');
-})
+//         res.end('<h1> Address: 19th KM Stone, NH-09, Ghaziabad, PIN - 201009. UP, India.</h1>');
+//     }
+//     else{
+//         res.end('404 Page Not Found');
+//     }
+// })
+// myserver.listen(8000,()=>{
+//     console.log('Server is running on port 8000');
+// })
 
+const fs = require('fs');
+
+// fs.writeFileSync("hello.txt", "This is Unnati Mishra", (err, result) => {
+//     if (err) {
+//         console.log("error",err);
+//     }
+//     else {
+//         console.log(result);
+//     }
+// })
+//const result =fs.readFileSync("hello.txt", "utf-8");
+//console.log(result);
+fs.appendFileSync("hello.txt", ` I am a student of ABES ENGINEERING COLLEGE`);
